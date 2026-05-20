c610c077ba469a54840a84d6b5c93b9ae7f7f310
TytanowyT4mpon <fak1223@gmail.com>
2026-05-21 01:28:43 +0200

    FIX: Mock Barber

--- mocks/barbers.ts
+++ mocks/barbers.ts
@@ -1,18 +1,21 @@
 import { Barber } from './../types/barber';
+import { ServicesMock } from './services';
+
+const s = (id: number) => ServicesMock.find(service => service.id === id)!;
 
 export const BarbersMock: Barber[] = [
     {
         id: 1,
         photo_URL: '/examples/barberExample1.jpg',
         name: 'Andress Gill',
-        services: [2, 4],
+        services: [s(2), s(4)],
         experience: '5+ years',
         rating: 4.3,
         freeDate: [
-            '2026-05-18', 
-            '2026-05-19', 
-            '2026-05-20', 
-            '2026-05-21', 
+            '2026-05-18',
+            '2026-05-19',
+            '2026-05-20',
+            '2026-05-21',
             '2026-05-22'
         ],
         freeHours: ['09:00', '10:30', '13:00', '14:30', '16:00', '17:30']
@@ -21,14 +24,14 @@ export const BarbersMock: Barber[] = [
         id: 2,
         photo_URL: '/examples/barberExample3.jpg',
         name: 'Adriana Lu',
-        services: [1, 2, 4],
+        services: [s(1), s(2), s(4)],
         experience: '2+ years',
         rating: 4.1,
         freeDate: [
-            '2026-05-20', 
-            '2026-06-12', 
-            '2026-06-23', 
-            '2026-05-22', 
+            '2026-05-20',
+            '2026-06-12',
+            '2026-06-23',
+            '2026-05-22',
             '2026-05-26'
         ],
         freeHours: ['10:00', '11:30', '13:00', '14:30', '16:00', '17:30']
@@ -37,14 +40,14 @@ export const BarbersMock: Barber[] = [
         id: 3,
         photo_URL: '/examples/barberExample2.jpg',
         name: 'Antony Herrey',
-        services: [1, 3, 4],
+        services: [s(1), s(3), s(4)],
         experience: '7+ years',
         rating: 4.9,
         freeDate: [
-            '2026-05-18', 
-            '2026-05-19', 
-            '2026-06-24', 
-            '2026-06-25', 
+            '2026-05-18',
+            '2026-05-19',
+            '2026-06-24',
+            '2026-06-25',
             '2026-06-27'
         ],
         freeHours: ['08:30', '10:30', '13:20', '14:30', '17:00', '19:30']
